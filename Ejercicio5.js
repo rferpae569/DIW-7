eee88d5f7acc89607a8e2cfddb35ae66c6b07abd
rferpae569 <rferpae569@g.educaand.es>
2022-11-15 21:05:12 +0100

    DIW7EjerciciosRubenFernandez

---
+++ Ejercicio5.js
@@ -0,0 +1,7 @@
+function ver(evt){
+
+    evt.preventDefault();
+    alert(document.getElementById("nombre").value);
+    alert(document.getElementById("email").value);
+    alert(document.getElementById("edad").value);
+}
